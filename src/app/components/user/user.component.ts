783ca2344611a869b5684fae3c63ad98c233bec7
Ferhat Avdic <ferhat.avdic@gmail.com>
2018-05-02 22:45:33 +0200

    code cleaned up

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -5,12 +5,6 @@ import { User } from '../../models/user';
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
-  // template: '<h2>Hello World</h2>'
-  // styles: [`
-  // h2{
-  //   color: blue;
-  // }
-  // `]
 })
 
 export class UserComponent implements OnInit{
@@ -27,12 +21,7 @@ export class UserComponent implements OnInit{
     this.user = {
       firstName: 'John',
       lastName: 'Doe',
-      age: 30,
-      address: {
-        street: '50 Main st',
-        city: 'Boston',
-        state: 'MA'
-      }
+      email: 'john@gmail.com'
     }
   }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,7 +7,7 @@ import { User } from '../../models/user';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  user: User = this.initUser();
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
@@ -20,94 +20,57 @@ export class UsersComponent implements OnInit {
 
   ngOnInit() {
     return new Promise( (resolve, reject) => {
-      // this.setCurrentClasses();
-      // this.setCurrentStyles();
       setTimeout(() => {
         this.users = [
           {
             firstName: 'John',
             lastName: 'Doe',
-            age: 30,
-            address: {
-              street: '50 Main st',
-              city: 'Boston',
-              state: 'MA'
-            },
-            // image: 'http://lorempixel.com/600/600/people/3',
+            email: 'john@gmail.com',
             isActive: true,
-            // balance: 100,
             registered: new Date('01/02/2018 08:30:00'),
             hide: true
           },
           {
             firstName: 'Kevin',
             lastName: 'Johnson',
-            age: 34,
-            // address: {
-            //   street: '20 School st',
-            //   city: 'Lynn',
-            //   state: 'MA'
-            // }
-            // image: 'http://lorempixel.com/600/600/people/2',
+            email: 'kevin@gmail.com',
             isActive: false,
-            // balance: 200,
             registered: new Date('03/11/2017 06:30:00'),
             hide: true
           },
           {
             firstName: 'Karen',
             lastName: 'Williams',
-            // age: 20,
-            address: {
-              street: '55 Mill st',
-              city: 'Miami',
-              state: 'FL'
-            },
-            // image: 'http://lorempixel.com/600/600/people/1',
+            email: 'karen@gmail.com',
             isActive: true,
-            // balance: 50,
             registered: new Date('11/02/2015 12:30:00'),
             hide: true
           }
-        ]
+        ];
         this.loaded = true;
         resolve({status: 'loaded', code: 1});
-      }, 2000);
+      }, 1000);
     });
-    // .then( (data:any) => { 
-    //   console.log(data.status);
-    //   this.addUser({firstName: 'Maximillian',lastName: 'Rowanson',age: 28,address: {street: '13 Mill st',city: 'New York',state: 'NY'}});
-    // });
-    
   }
 
-  fireEvent(e){
-    console.log(e.target.value);
+  initUser() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
   }
-
   onSubmit(e){
     console.log(123);
     e.preventDefault();
   }
-
-  // toggleHide(user){
-  //   user.hide = !user.hide;
+  // addUser() {
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+  //   this.users.unshift(this.user);
+  //   this.user = this.initUser();
   // }
 
-  // addUser(user:User){
-  //   this.users.push(user);
-  // }
-  // setCurrentClasses(){
-  //   this.currentClasses = {
-  //     'btn-success': this.enableAdd,
-  //     'big-text': this.showExtended
-  //   }
-  // }
-  // setCurrentStyles(){
-  //   this.currentStyles = {
-  //     'font-size': this.showExtended ? '':'40px'
-  //   }
-  // }
-
+ 
 
 }
